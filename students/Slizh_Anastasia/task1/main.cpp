d6095336ca35cf99fe38437675ef2caab9b5c86d
Slizh_Anastasia <anastasiya.slizh@mail.ru>
2018-02-24 01:35:02 +0300

    Я уже все перепробовала, не понимаю почему он не работает, извините

--- students/Slizh_Anastasia/task1/main.cpp
+++ students/Slizh_Anastasia/task1/main.cpp
@@ -4,10 +4,8 @@
 #include <stdio.h>
 #include <time.h>
 
-
-
 using namespace std;
-void Enter(int *_hour, int *_minute, int *_second);
+void Enter(int *_h, int *_min, int *_sec);
 class Time {
 private:
 	int hour, minute, second;
@@ -19,107 +17,112 @@ public:
 		second = obj.second;
 		return *this;
 	}
-	void timeDiff(int &_hour, int &_min, int &_sec);
-	void changeTime1(int _hour, int _min, int _sec);
-	void changeTime2(int _hour, int _min, int _sec);
+	void timeDiff(int _hour, int _minute, int _second);
+	void changeTime1(int _hour, int _minute, int _second);
+	void changeTime2(int _hour, int _minute, int _second);
 };
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	int k = 0, choice, choice1;
-	int _hour = 0, _minute = 0, _second = 0;
-	int hour = 0, minute = 0, second = 0;
-	Time T1;
+	int k = 0, choice, choice1, p;
+	int _h = 0, _min = 0, _sec = 0;
 	system("cls");
-	cout << "Задать время:" << endl;
-	Enter(&_hour, &_minute, &_second);
+	Time T1;
+	cout << "Задать время" << endl;
+	Enter(&_h, &_min, &_sec);
+
 	system("pause");
 	system("cls");
 
 
 	while (k != 3) {
 		system("cls");
-		cout << "Текущее время: ";
-		cout << _hour << ":" << _minute << ":" << _second << endl;
+		cout << "Заданное время: ";
+		cout << hous << ":" << minute << ":" << second << endl;
+		system("pause");
 		cout << "1. Рассчитать разницу между введеным и заданным" << endl;
-		cout << "2 Время перемещения" << endl;
+		cout << "2  Изменить время" << endl;
 		cout << "3. Выход" << endl;
-		cout << "Ваш выбор: ";
 		cin >> choice;
 		switch (choice) {
 		case 1:
 		{
-			Enter(&_hour, &_minute, &_second);
-			T1.timeDiff(hour, minute, second);
+			Enter(&_h, &_min, &_sec);
+			T1.timeDiff(_h, _min, _sec);
 			cout << "Разница между введеным и заданным ";
-			cout << hour << ":" << minute << ":" << second << endl;
+			cout << _h << ":" << _min << ":" << _sec << endl;
 			system("pause");
 			break;
 		}
 		case 2:
 		{
 			cout << "Введите время которое вы хотите изменить" << endl << endl;
-			Enter(&_hour, &_minute, &_second);
-			cout << "Выбрать направление" << endl;
-			cout << "1. Вперед" << endl;
-			cout << "2. Назад" << endl;
-			cout << "Ваш выбор: ";
-			cin >> choice1;
-			switch (choice1) {
-			case 1:
+			Enter(&_h, &_min, &_sec);
+			while (p != 3)
 			{
-				T1.changeTime1(_hour, _minute, _second);
+				cout << "Выбрать направление" << endl;
+				cout << "1. Вперед" << endl;
+				cout << "2. Назад" << endl;
+				cout << "3. Выйти в прошлое меню" << endl;
+				cin >> choice1;
+				switch (choice1) {
+				case 1:
+				{
+					T1.changeTime1(_h, _min, _sec);
+				}
+				case 2:
+				{
+					T1.changeTime2(_h, _min, _sec);
+				}
+				case 3: {
+					p = 3; }
+				}
+				system("cls");
 			}
-			case 2:
-			{
-				T1.changeTime2(_hour, _minute, _second);
-			}
-			break;
-			}
-		}
+			return;
 		case 3:
 		{
 			return 0;
 		}
 		}
+		}
 	}
 }
 
-void Enter(int &_hour, int &_minute, int &_second)
+void Enter(int *h, int *min, int *sec)
 {
 	cout << "Укажите часы: ";
-	cin >> _hour;
-	if ((_hour > 23) || (_hour<0))
+	cin >> *h;
+	if ((*h > 23) || (*h<0))
 	{
 		cout << "Ошибка";
-		return;
+		return 0;
 	}
 	cout << "Укажите минуты: ";
-	cin >> _minute;
-	if ((_minute > 59) || (_minute<0))
+	cin >> *min;
+	if ((*min > 59) || (*min<0))
 	{
 		cout << "Ошибка";
-		return;
+		return 0;
 	}
 	cout << "Укажите секунды: ";
-	cin >> _second;
-	if ((_second > 59) || (_second<0))
+	cin >> *sec;
+	if ((*sec > 59) || (*sec<0))
 	{
 		cout << "Ошибка";
-		return;
+		return 0;
 	}
 }
 
-void Time::timeDiff(int &_hour, int &_minute, int &_second)
+void Time::timeDiff(int *_hour, int *_minute, int *_second)
 {
 	int second1 = hour * 3600 + minute * 60 + second;
-	int second2 = _hour * 3600 + _minute * 60 + _second;
-	int diff = second1 - second2;
-	_minute = diff / 60;
-	_second = diff - _minute * 60;
-	diff = _minute;
-	_hour = diff / 60;
-	_minute = diff - _hour * 60;
+	int second2 = *_hour * 3600 + *_minute * 60 + *_second; 3
+		int diff = abs(second1 - second2);
+	*_hour = diff / 3600;
+	diff = diff - *_hour * 3600;
+	*_minute = diff / 60;
+	*_second = diff % 60;
 }
 
 void Time::changeTime1(int _hour, int _minute, int _second)
@@ -127,6 +130,7 @@ void Time::changeTime1(int _hour, int _minute, int _second)
 	hour -= _hour;
 	minute -= _minute;
 	second -= _second;
+	cout << hour << ":" << minute << ":" << second << endl;
 }
 
 void Time::changeTime2(int _hour, int _minute, int _second)
@@ -134,4 +138,5 @@ void Time::changeTime2(int _hour, int _minute, int _second)
 	hour -= _hour;
 	minute -= _minute;
 	second -= _second;
+	cout << hour << ":" << minute << ":" << second << endl;
 }
